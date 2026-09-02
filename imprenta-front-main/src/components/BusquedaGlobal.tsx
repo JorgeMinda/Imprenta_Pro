ed88d6a850ca2a36c22ed80dfa7854f54dba0494
Jorge Leonardo Minda <jls.minda@yavirac.edu.ec>
2026-09-02 18:55:31 -0500

    fix: typo motion-div -> motion.div in BusquedaGlobal.tsx

--- imprenta-front-main/src/components/BusquedaGlobal.tsx
+++ imprenta-front-main/src/components/BusquedaGlobal.tsx
@@ -227,7 +227,7 @@ export default function BusquedaGlobal() {
             />
 
 {/* Panel */}
-            <motion-div
+            <motion.div
               key="busqueda-panel"
               initial={{ opacity: 0, y: -20, scale: 0.97 }}
               animate={{ opacity: 1, y: 0,   scale: 1 }}
